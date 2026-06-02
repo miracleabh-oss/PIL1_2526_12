eb83e7393fb88e396195e949add0f9bcacb508b8
Miracle <miracleabh@gmail.com>
2026-06-02 22:32:07 +0100

    Ajout de la page de connexion

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 from flask_socketio import SocketIO
 import pymysql
 
@@ -15,9 +15,22 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 socketio = SocketIO(app)
 
+class Utilisateur(UserMixin, db.Model):
+    __tablename__ = 'utilisateur'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100))
+    prenom = db.Column(db.String(100))
+    email = db.Column(db.String(150), unique=True)
+    telephone = db.Column(db.String(20), unique=True)
+    mot_de_passe = db.Column(db.String(255))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Utilisateur.query.get(int(user_id))
+
 @app.route('/')
 def accueil():
-    return "MentorLink fonctionne !"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
